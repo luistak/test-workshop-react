799a81ec1737e35867fcec410cdc54a582806f5f
Luis Takahashi <takahashihideki408@gmail.com>
2020-12-12 15:46:22 -0300

    Added initial solution

--- src/custom-matcher.test.js
+++ src/custom-matcher.test.js
@@ -6,12 +6,28 @@ import { Counter } from './counter';
 
 // Following this example: https://jestjs.io/docs/en/expect#expectextendmatchers
 expect.extend({
-  toHaveTextContent() {
-    // TODO: Implement
+  toHaveTextContent(received, text) {
+    const pass = received.textContent === text;
+
+    // Using "this.utils": https://jestjs.io/docs/en/expect#thisutils
+    const expected = this.utils.printExpected(received);
+
+    if (!pass) {
+      return {
+        message: () =>
+          `expected ${expected} to have text content ${text} but got ${received.textContent}`,
+        pass,
+      };
+    }
+
+    return {
+      message: () =>
+        `expected ${expected} NOT to have text content ${text} but got ${received.textContent}`,
+      pass,
+    };
   }
 });
 
-// Make this test pass!
 test('renders counter', async () => {
   render(<Counter />);
   const count = screen.getByText('0');
