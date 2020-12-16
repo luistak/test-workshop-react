6b414e7d7e225737730d9c575950efefda3927bc
Luis Takahashi <takahashihideki408@gmail.com>
2020-12-16 01:06:30 -0300

    Added initial exercise

--- src/__tests__/app.integration.test.js
+++ src/__tests__/app.integration.test.js
@@ -1,64 +1,29 @@
-import user from '@testing-library/user-event';
-import { render, screen, waitFor } from '@testing-library/react';
-
-import * as api from '../api';
+import { render } from '@testing-library/react';
 
 /**
- * Change between these imports to check if your tests are resilient
+ * TODO: Switch between these imports to check if your tests are resilient
  */
 import { App } from '../app';
 // import { App } from '../app-redux';
 
-beforeAll(() => {
-  jest.spyOn(api, 'checkOut');
-  jest.spyOn(window, 'alert');
-})
-
-afterEach(() => {
-  api.checkOut.mockClear();
-  window.alert.mockClear();
-})
-
-afterAll(() => {
-  api.checkOut.mockRestore();
-  window.alert.mockRestore();
-})
-
-test('order check out', async () => {
-  api.checkOut.mockResolvedValue({ success: true });
+/**
+ * TODO: Automate every instruction following the @testing-library best practices
+ */
+test('Order check out', async () => {
+  // TODO: Mock necessary api's
 
   render(<App />);
 
-  // Assert initial page
-  const heading = screen.getByRole('heading',{ name: /items/i });
-  expect(heading).toBeInTheDocument();
+  // TODO: Assert initial page
 
-  // Find and navigate do first item details
-  const items = screen.getAllByTestId('item');
-  const firstItem = items[0];
-  expect(firstItem).toBeInTheDocument();
+  // TODO: Find and navigate to first item details
 
-  user.click(firstItem);
+  // TODO: Add item to cart
 
-  // Add item to cart
-  const itemInput = screen.getByLabelText(/quantity/i);
-  user.type(itemInput, '12');
+  // TODO: Go to Cart
 
-  const addToCartButton = screen.getByRole('button', { name: /add to cart/i });
-  user.click(addToCartButton);
+  // TODO: Check out the order and assert its calls
 
-  // Go to Cart
-  const cartLink = screen.getByText('Cart');
-  user.click(cartLink);
+  // TODO: Assert initial page redirect
 
-  // Checkout
-  const checkOutButton = screen.getByText(/check out/i);
-  expect(checkOutButton).toBeInTheDocument();
-  user.click(checkOutButton);
-
-  await waitFor(() => expect(api.checkOut).toHaveBeenCalled());
-  await waitFor(() => expect(window.alert).toHaveBeenCalledWith('Order created successfully!'));
-
-  // Assert initial page redirect
-  expect(screen.getByRole('heading',{ name: /items/i })).toBeInTheDocument();
 });
--- src/__tests__/customRender.test.js
+++ src/__tests__/customRender.test.js
@@ -1,24 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 
-import { Cart, EcommerceProvider } from '../app';
-
-const Wrapper = ({ children }) => (
-  <EcommerceProvider>
-    <MemoryRouter>
-      {children}
-    </MemoryRouter>
-  </EcommerceProvider>
-)
-
-// Custom render docs: https://testing-library.com/docs/react-testing-library/setup#custom-render
-const customRender = (ui, options) => render(ui, { wrapper: Wrapper, ...options })
+import { Cart } from '../app';
 
 /**
  * Make it works using a Wrapper or a customRender
  */
 test('order check out', async () => {
-  customRender(<Cart />);
+  render(<Cart />);
 
   /**
    * Change only the code above
